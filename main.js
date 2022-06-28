b091a32300086f79af6fe75dcebf51ea3bd66db2
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-06-28 13:51:29 -0400

    commit

--- main.js
+++ main.js
@@ -4,13 +4,24 @@ const rosterDiv = document.querySelector('#roster')
 newBtn.addEventListener('click', () => {
    let num = prompt('Enter a Pokemon number')
    console.log('Number entered', num)
+   let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`
+    let img = document.createElement('img')
+    img.setAttribute('src', imgUrl)
+    img.setAttribute('class', 'roster-img')
+    let position = document.querySelector(`#pokemon-${userRoster.length+1}`)
+    position.append(img)
+    userRoster.push(num)
 })
+
+
 
 const pokemon = [
     {name: 'Bulbasaur', id:'001'},
     {name: 'Charmander', id:'004'},
     {name: 'Squirtle', id:'007'}   
 ]
+
+const userRoster = []
 
 //loop over id 
 // create html elemt
