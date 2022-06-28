b6b7bdbcb5d35a92bf019fcab2bebc9f833c6bb8
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-06-28 11:22:46 -0400

    commit

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-const pokemon = ['charmander', 'bulbasaur', 'squirtle']
-const pokemonIds = ['001', '002', '003']
 const containerDiv = document.querySelector('#container')
+
+const pokemon = [
+    {name: 'Bulbasaur', id:'001'},
+    {name: 'Charmander', id:'004'},
+    {name: 'Squirtle', id:'007'}   
+]
 
 //loop over id 
 // create html elemt
 // set elem
 //append elem
 
-pokemonIds.map((id)  => {
-    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-    console.log('imgUrl', imgUrl)
+pokemon.map((element, index)  => {
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
+    let h3 = document.createElement('h3')
+    h3.innerText = element.name
     let div = document.createElement('div')
     div.setAttribute('class', 'pokemon-card')
+    console.log(element)
     let img = document.createElement('img')
     img.src = imgUrl
-   div.append(img)
-   containerDiv.append(div)
+    div.append(img, h3)
+    containerDiv.append(div)
 })
+
